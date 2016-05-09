7cdc90fc9b37140bf459f27a9e77af9369fc388f
David Bankier <dbankier@hotmail.com>
2016-05-10 09:32:44 +1000

    get mishnah source for wikisource

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Mishnah Study Library
 
 This node package is used to generate Mishnah learning calendars. It was primarily
-built for [Mishnah Yomit](http://www.mishnahyomit.com) but can be used to build 
+built for [Mishnah Yomit](http://www.mishnahyomit.com) but can be used to build
 calendars for other learning structures.
 
 
@@ -16,7 +16,7 @@ $ npm install mishnah -S
 ```
 var mishnah = require("mishnah");
 
-//The total number of Mishanayot. 
+//The total number of Mishanayot.
 mishnah.total;
 
 //An array of the names of all the tractates.
@@ -28,7 +28,7 @@ mishnah.structure;
 
 ```
 
-## Functions 
+## Functions
 
 ### mishnah.getToday(date,pretty)
 
@@ -65,9 +65,9 @@ The argument is an optional object:
   {
      start: new Date(),  // starting date (defaults to  10 July 2010)
      per_day: 2,         // mishnayot to be learnt per day (default: 2)
-     cycles:  1,         // cycles to generate (default: 1) 
-     perakim:  false,    // build a perakim based learning schedule (default: false) 
-     sun2thurs:  false,  // build a schedule for Sunday to Thursday only (default: false) 
+     cycles:  1,         // cycles to generate (default: 1)
+     perakim:  false,    // build a perakim based learning schedule (default: false)
+     sun2thurs:  false,  // build a schedule for Sunday to Thursday only (default: false)
   }
 ```
 
@@ -83,7 +83,7 @@ The function returns the following array:
       t: 0, // tractate index (use the mishnah.names property)
       p: 1, // perek number
       m: 2, // mishnah number
-   }],  
+   }],
    date: Date, // date object
    english: "Monday, September 7th 2015" // nicely formated english date
    hebrew: "23 Elul 5775", // nicely formatted hebrew date
@@ -94,6 +94,22 @@ The function returns the following array:
 ### mishnah.prettyFormat(d)
 
 Returns a pretty format of a Mishnah range (as already described above).
+
+### mishnah.source(mishnah, callback)
+
+Returns the mishnah source/text in html format from wikisource.
+
+The mishnah object expects the following format:
+
+```
+{
+  t: 0, // tractate index (use the mishnah.names property)
+  p: 1, // perek number
+  m: 1, // mishnah number
+}
+```
+
+If a callback is not provided, a promise will be returned.
 
 ### Credits
 
--- index.js
+++ index.js
@@ -3,18 +3,20 @@ var fs = require("fs");
 var _ = require("underscore");
 var moment = require("moment");
 var eng2heb = require("./lib/eng2heb");
+var cheerio = require("cheerio");
+var request = require("request");
 
 // loading the data
-var json = JSON.parse(fs.readFileSync(path.join(__dirname, "data","mishnah.json")));
+var json = JSON.parse(fs.readFileSync(path.join(__dirname, "data", "mishnah.json")));
 var total_m = 0;
 var total_p = 0;
 //extract names
 var names = _.keys(json);
-
+var hebrew = JSON.parse(fs.readFileSync(path.join(__dirname, "data", "hebrew.json")));
 //count total mishnayot
-_.values(json).forEach(function(arr) {
+_.values(json).forEach(function (arr) {
   total_p += arr.length;
-  arr.forEach(function(value) {
+  arr.forEach(function (value) {
     total_m += parseInt(value, 10);
   });
 });
@@ -34,12 +36,12 @@ function increment(perakim) {
 
   if (+json[names[mas_index]][perek_index] === mish_index) {
     mish_index = 0;
-    perek_index ++;
+    perek_index++;
   }
-  if (json[names[mas_index]].length ===perek_index) {
+  if (json[names[mas_index]].length === perek_index) {
     mish_index = 0;
     perek_index = 0;
-    mas_index ++;
+    mas_index++;
     if (mas_index === names.length) {
       mas_index = 0;
     }
@@ -47,14 +49,14 @@ function increment(perakim) {
 }
 function buildList(perDay, perakim) {
   var ret = [];
-  for (var i = 0; i < (perakim ? total_p : total_m)/perDay; i++ ){
+  for (var i = 0; i < (perakim ? total_p : total_m) / perDay; i++) {
     var day = [];
-    day.push({t:mas_index,p:(perek_index+1),m:(mish_index+1)});
+    day.push({ t: mas_index, p: (perek_index + 1), m: (mish_index + 1) });
     if (perDay > 1) {
-      for (var j = 1; j<perDay; j++) {
+      for (var j = 1; j < perDay; j++) {
         increment(perakim);
       }
-      day.push({t:mas_index,p:(perek_index+1),m:(mish_index+1)});
+      day.push({ t: mas_index, p: (perek_index + 1), m: (mish_index + 1) });
     }
     increment(perakim);
     ret.push(day);
@@ -62,17 +64,17 @@ function buildList(perDay, perakim) {
   return ret;
 }
 
-function prettyLine(d, display_only_first,show_perek, only_perakim) {
-  return ((d.p === 1 && d.m === 1) || !display_only_first ? names[d.t] + " " : "")  +
-    (d.m === 1 || show_perek ? d.p + (!only_perakim  ? ":":"")  :"") +
+function prettyLine(d, display_only_first, show_perek, only_perakim) {
+  return ((d.p === 1 && d.m === 1) || !display_only_first ? names[d.t] + " " : "") +
+    (d.m === 1 || show_perek ? d.p + (!only_perakim ? ":" : "") : "") +
     (!only_perakim ? d.m : "");
 }
 
 function prettyFormat(d, display_only_first, only_perakim) {
-  return prettyLine(d[0],display_only_first,true, only_perakim) +
+  return prettyLine(d[0], display_only_first, true, only_perakim) +
     (d[1] !== undefined ?
-    (d[1].t  !== d[0].t  ?  " - " : "-")  +
-    prettyLine(d[1],d[1].t === d[0].t,d[1].t !== d[0].t || d[1].p !== d[0].p, only_perakim): "");
+      (d[1].t !== d[0].t ? " - " : "-") +
+      prettyLine(d[1], d[1].t === d[0].t, d[1].t !== d[0].t || d[1].p !== d[0].p, only_perakim) : "");
 }
 
 exports.structure = json;
@@ -81,23 +83,23 @@ exports.total = total_m;
 exports.prettyFormat = prettyFormat;
 
 var mishnah_yomit = buildList(2);
-exports.getToday = function(_date, pretty) {
+exports.getToday = function (_date, pretty) {
   var start = moment([2010, 6, 4]);
   var date = moment(_date);
   var mish = mishnah_yomit[+date.diff(start, 'days') % mishnah_yomit.length];
   return pretty ? prettyFormat(mish) : mish;
 };
 
-exports.buildCalendar=function(o)  {
+exports.buildCalendar = function (o) {
   mas_index = 0;
   perek_index = 0;
   mish_index = 0;
 
-  var day = new moment(o.start||moment([2010, 6, 4]));
-  var list = buildList(o.per_day||2, o.perakim);
+  var day = new moment(o.start || moment([2010, 6, 4]));
+  var list = buildList(o.per_day || 2, o.perakim);
   var data = [];
   function addDay(d) {
-    var ret =  {
+    var ret = {
       d: d,
       pretty: prettyFormat(d, false, o.perakim),
       date: new Date(day.toDate()),
@@ -105,8 +107,8 @@ exports.buildCalendar=function(o)  {
       hebrew: eng2heb.convert(day.toDate()),
     };
     day.add(1, 'd');
-    if (o.sun2thurs && day.day() === 5)  { day.add(1, 'd'); }
-    if (o.sun2thurs && day.day() === 6)  { day.add(1, 'd'); }
+    if (o.sun2thurs && day.day() === 5) { day.add(1, 'd'); }
+    if (o.sun2thurs && day.day() === 6) { day.add(1, 'd'); }
 
     data.push(ret);
   }
@@ -115,6 +117,42 @@ exports.buildCalendar=function(o)  {
   }
   return data;
 };
+
+exports.source = function (o, callback) {
+  var masechet = hebrew.names[o.t];
+  var perek = hebrew.values[o.p];
+  var mishnah = hebrew.values[o.m];
+  var encoded_page = encodeURI('משנה_' + masechet + "_" + perek + "_" + mishnah);
+  var url = 'https://he.wikisource.org/w/api.php?action=parse&format=json&section=0&prop=text&page=' + encoded_page;
+  function getMishnah(cb) {
+    request(url, function (err, reponse, body) {
+      if (err) { return cb(err) }
+      var json = JSON.parse(body);
+      var content = json.parse.text['*'];
+      var $ = cheerio.load(content);
+
+      var mishnah_div = $("div").filter(function () {
+        return $(this).css("font-size") == "120%"
+      });;
+
+      var complete = `
+      <div style="direction: rtl;">${mishnah_div.html()}</div>
+      <small>Source <a href="https://he.wikisource.org/wiki/${encoded_page}">Wikisource</small>
+      `
+      cb(null, complete);
+    })
+  }
+  if (callback) {
+    return getMishnah(callback);
+  }
+  return new Promise((resolve, reject) => {
+    getMishnah((err, content) => {
+      if (err) { return reject(err)}
+      resolve(content);
+    })
+  });
+
+}
 
 
 
