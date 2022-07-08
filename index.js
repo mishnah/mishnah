b69fc52dfafdd5dbdfc51598abeb391afed28a3d
David Bankier <dbankier@hotmail.com>
2022-07-08 13:49:57 +0300

    Added `buildTargetCalender` function based on start date, current mishnah and desired end date. Needs documentation.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var fs = require('fs');
 var _ = require('underscore');
 var moment = require('moment');
 var eng2heb = require('./lib/eng2heb');
-var cheerio = require('cheerio');
 var request = require('request');
 
 // loading the data
@@ -31,34 +30,31 @@ var seder = JSON.parse(
   fs.readFileSync(path.join(__dirname, 'data', 'sedarim.json'))
 );
 
-//build daily data
-var mas_index = 0;
-var perek_index = 0;
-var mish_index = 0;
-
-// basic incrementer
-function increment(perakim) {
+//basic incrementer
+function increment(index, perakim) {
   if (perakim) {
-    perek_index++;
+    index.p++;
   } else {
-    mish_index++;
+    index.m++;
   }
 
-  if (+json[names[mas_index]][perek_index] === mish_index) {
-    mish_index = 0;
-    perek_index++;
+  if (+json[names[index.t]][index.p] === index.m) {
+    index.m = 0;
+    index.p++;
   }
-  if (json[names[mas_index]].length === perek_index) {
-    mish_index = 0;
-    perek_index = 0;
-    mas_index++;
-    if (mas_index === names.length) {
-      mas_index = 0;
+  if (json[names[index.t]].length === index.p) {
+    index.m = 0;
+    index.p = 0;
+    index.t++;
+    if (index.t === names.length) {
+      index.t = 0;
     }
   }
+  return index;
 }
 function buildList(perDay, perakim, sederName) {
   var ret = [];
+  let index = { t: 0, p: 0, m: 0 };
 
   var allowedMasechtos = [];
   if (sederName && seder[sederName]) {
@@ -71,25 +67,25 @@ function buildList(perDay, perakim, sederName) {
     // Limit the schedule to a particular seder only if requested
     if (sederName && allowedMasechtos.length > 0) {
       while (
-        allowedMasechtos.indexOf(mas_index) === -1 &&
-        mas_index < names.length
+        allowedMasechtos.indexOf(index.t) === -1 &&
+        index.t < names.length
       ) {
-        mas_index++;
+        index.t++;
       }
-      if (allowedMasechtos.indexOf(mas_index) === -1) {
+      if (allowedMasechtos.indexOf(index.t) === -1) {
         break;
       }
     }
 
     var day = [];
-    day.push({ t: mas_index, p: perek_index + 1, m: mish_index + 1 });
+    day.push({ t: index.t, p: index.p + 1, m: index.m + 1 });
     if (perDay > 1) {
       for (var j = 1; j < perDay; j++) {
-        increment(perakim);
+        index = increment(index, perakim);
       }
-      day.push({ t: mas_index, p: perek_index + 1, m: mish_index + 1 });
+      day.push({ t: index.m, p: index.p + 1, m: index.m + 1 });
     }
-    increment(perakim);
+    index = increment(index, perakim);
     ret.push(day);
   }
   return ret;
@@ -132,10 +128,6 @@ exports.getToday = function (_date, pretty) {
 };
 
 exports.buildCalendar = function (o) {
-  mas_index = 0;
-  perek_index = 0;
-  mish_index = 0;
-
   var day = new moment(o.start || moment([2010, 6, 4]));
 
   var list = buildList(o.per_day || 2, o.perakim, o.seder);
@@ -164,6 +156,78 @@ exports.buildCalendar = function (o) {
   }
   return data;
 };
+
+exports.buildTargetCalendar = function (start_date, end_date, current_d) {
+  var start = moment(start_date).startOf('day');
+  var end = moment(end_date).startOf('day');
+  var days = end.diff(start, 'days') + 1;
+
+  let mishnah_left = 0;
+  Object.keys(json).forEach(function (masechta, idx) {
+    if (idx >= current_d.t) {
+      let perakim = [...json[masechta]];
+      if (idx === current_d.t) {
+        perakim = perakim.slice(current_d.p - 1);
+        perakim[0] = perakim[0] - (current_d.m - 1);
+      }
+      mishnah_left += perakim.reduce((a, b) => a + +b, 0);
+    }
+  });
+
+  let average = mishnah_left / days;
+  let index = { t: current_d.t, p: current_d.p - 1, m: current_d.m - 1 };
+  let running_average = 0;
+  let list = [];
+  for (var i = 0; i < days; i++) {
+    let step;
+    if (running_average < average) {
+      step = Math.ceil(average);
+    } else {
+      step = Math.floor(average);
+    }
+
+    var day = [];
+    day.push({ t: index.t, p: index.p + 1, m: index.m + 1 });
+
+    if (step > 1) {
+      for (var j = 1; j < step; j++) {
+        index = increment(index);
+      }
+      day.push({ t: index.t, p: index.p + 1, m: index.m + 1 });
+    }
+    list.push(day);
+
+    index = increment(index);
+    running_average = (running_average * i) / (i + 1) + step / (i + 1);
+  }
+
+  var day = new moment(start_date);
+
+  var data = [];
+  function addDay(d) {
+    var ret = {
+      d: d,
+      pretty: prettyFormat(d),
+      date: new Date(day.toDate()),
+      english: day.format('dddd, MMMM Do YYYY'),
+      hebrew: eng2heb.convert(day.toDate())
+    };
+    day.add(1, 'd');
+    data.push(ret);
+  }
+  list.forEach(addDay);
+  return { average, data };
+};
+
+let in_a_year = exports.buildTargetCalendar(
+  new Date(),
+  moment().add(1, 'year').toDate(),
+  {
+    t: 0,
+    p: 1,
+    m: 1
+  }
+);
 
 exports.source = function (o, callback) {
   var masechet = hebrew.names[o.t];
